17bfcb0a9482971fe1113c49f07d1645f852824d
Presotn Brubaker <brubakerpreston@gmail.com>
2025-01-31 18:50:30 -0600

    m

--- src/agents.c
+++ src/agents.c
@@ -59,7 +59,7 @@ void update_agents(Agent agents[]) {
             agents[i].y = 0.0f;
         }
 
-        if (rand() % 100 < 40 && agents[i].food > 0) {
+        if (rand() % 100 < 20 && agents[i].food > 0) {
             agents[i].food--;
         }
         if (rand() % 2 == 0) {
